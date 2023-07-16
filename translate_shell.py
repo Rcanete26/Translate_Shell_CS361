ee25f59483d69e0f4b6798fdd450bddf7adcd8d8
Ryan Canete <caneter@oregonstate.edu>
2023-07-15 23:21:09 -0700

    Added initial warning, instructions, and intro to the program and features. Also now has the ability to show the last translation if there was one

--- translate_shell.py
+++ translate_shell.py
@@ -7,13 +7,20 @@ from simple_term_menu import TerminalMenu
 # pip install googletrans==4.0.0rc1
 # python3 -m pip install simple-term-menu
 supported_languages = googletrans.LANGUAGES
+intro = 'This program is a script that allows you to to easily translate through your shell! The input should be auto detected and will translate to english by default. Select Change Destination Language if you would like to translate to a different langauge! \n'
+warning = "Please be careful when using this project! Longer prompts of text will take longer to translate so please be patient. Please do not send anything over 15k charaters or the program might fail or cause instability.\n"
+more_info_app = "This program was made using two packages! Simple Term Menu allows this program to have a selectable menu from the command line. GoogleTras is the package that allows this program to provide the translations! This app is not associated with google.\n "
+instructions = " Use the up and down arrow keys to navigate through the menu and enter to select.\n"
 
 def main():
     Running = True
     destination_language = 'en'
-    
+    last_translation = "There have been no translations yet!"
+    print(intro)
+    print("----------------")
+    print(warning)
     while Running == True:
-        main_options = ['Translate', 'More Info','Change Destination Language','Quit']
+        main_options = ['Translate', 'More Info','Show Last Translation', 'Change Destination Language','Quit']
         main_menu = TerminalMenu(main_options)
         menu_entry = main_menu.show()
         if main_options[menu_entry] == "Quit":
@@ -25,7 +32,13 @@ def main():
             destination_language = new_language
 
         elif main_options[menu_entry] == 'Translate':
-            translation_part(destination_language)
+            last_translation = translation_part(destination_language)
+
+        elif main_options[menu_entry] == 'Show Last Translation':
+            print(last_translation)
+        
+        elif main_options[menu_entry] == 'More Info':
+            print(more_info_app)
 
 def change_language(current_language):
     for languages in supported_languages:
@@ -39,6 +52,7 @@ def translation_part(destion_lang):
             source_text = input("What would you like the translate?: ")
             translation = translator.translate(source_text, destion_lang)
             print(translation.text)
+            return (f"{source_text} = ({destion_lang})-{translation.text}")
 
 if __name__ == '__main__':
     main()
